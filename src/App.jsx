4a5dcd69488fbc60e01c253b7c2dd5c27f1a084c
huma515 <humahashim11@gmail.com>
2026-03-07 07:36:02 +0500

    add award section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Clients from './components/Clients '
 import Promise from './components/Promise'
+import Awards from './components/Awards.JSX'
 
 function App() {
   
@@ -16,6 +17,7 @@ function App() {
     <Hero/>
     <Clients/>
     <Promise/>
+    <Awards/>
     </>
   )
 }
